68432c324eaaae743124263e309ced52143b4cf0
rongmic <rongmic@gmail.com>
2013-09-05 09:10:21 +0800

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mlog::Application.config.secret_key_base = 'c55341b9bc4ad442d52c46be1887c8a97ffa93bc1f4ef50c51740598295e5830b70e1d4d9635247eb06de37579c85254a7711dee3079d0ed8784a44e4c1badcb'
