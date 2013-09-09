168760280e5ce7d32c6e1ad7c79cc348b734cca0
rongmic <rongmic@gmail.com>
2013-09-09 17:09:26 +0800

    admin articles

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,4 +1,33 @@
 class Admin::ArticlesController < Admin::BaseAdminController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+    @categories = Category.all
+  end
+
+  def create
+    @article = Article.new(article_params.merge!(status: 1))
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to admin_articles_path }
+      else
+        format.text { "Occure Error while creating a new article" }
+      end
+    end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content, :top, :category_id, :status)
   end
 end
